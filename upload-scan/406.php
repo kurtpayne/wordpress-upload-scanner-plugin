8cdebc25b2a740be67635db4990056ad22414d9a
kurtpayne <kpayne@gmail.com>
2012-03-04 11:46:34 -0700

    Faking an apache 406 page

---
+++ upload-scan/406.php
@@ -0,0 +1,8 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>406 Not Acceptable</title>
+</head><body>
+<h1>Not Acceptable</h1>
+<p>An appropriate representation of the requested resource <?php echo $_SERVER['SCRIPT_NAME']; ?> 
+ could not be found on this server.</p>
+</body></html>
