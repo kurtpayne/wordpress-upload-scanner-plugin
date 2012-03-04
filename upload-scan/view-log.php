0202f96dd390398072b7fc6713e970da0dfc0879
kurtpayne <kpayne@gmail.com>
2012-03-04 11:44:54 -0700

    Added log viewer

---
+++ upload-scan/view-log.php
@@ -0,0 +1,25 @@
+<?php
+
+if ( !defined( 'ABSPATH') ) {
+    exit();
+}
+
+?>
+<div class="wrap">
+	<div id="icon-tools" class="icon32"><br/></div>
+	<h2>Upload Scan Log</h2>
+	<textarea id="upload-scan-view-log" name="upload_scan_view_log" style="font-family: monospace; width: 90%; height: 500px; margin: 25px auto auto 25px;"><?php
+		if ( $this->does_log_file_exist() ) {
+			$fp = fopen( get_option( 'upload-scan_onfail_log_file' ) , 'r' );
+			while ( ( $line = fgetss( $fp ) )!== false ) {
+				echo htmlentities( $line );
+			}
+			fclose( $fp );
+		} else {
+			echo "Log file does not exist or is not readable";
+		}
+	?></textarea>
+	<p>
+	<a href="<?php echo remove_query_arg( '__action' ); ?>" class="button-secondary">Back</a>
+	</p>
+</div>
