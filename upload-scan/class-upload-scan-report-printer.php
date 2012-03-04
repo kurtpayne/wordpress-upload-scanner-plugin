97a3242f2c58418a9d9c3dcde5e75c6c5f5e7669
kurtpayne <kpayne@gmail.com>
2012-03-04 11:46:16 -0700

    Different formats for e-mail messages and log messages

--- upload-scan/class-upload-scan-report-printer.php
+++ upload-scan/class-upload-scan-report-printer.php
@@ -25,4 +25,25 @@ abstract class Upload_Scan_Report_Printer {
 	public function setReport( Upload_Scan_Report $report ) {
 		$this->_report = $report;
 	}
+	
+	/**
+	 * Get the current URL
+	 * @return string
+	 */
+	public function getCurrentURL() {
+		$url = $_SERVER['REQUEST_URI'];
+		$parts = parse_url( $url );
+		if ( isset( $parts['scheme'] ) ) {
+			$scheme = $parts['scheme'];
+		} elseif ( is_ssl() ) {
+			$scheme = 'https';
+		} else {
+			$scheme = 'http';
+		}
+		return sprintf('%s://%s%s',
+			$scheme,
+			( isset( $parts['host'] ) ? $parts['host'] : $_SERVER['HTTP_HOST'] ),
+			$_SERVER['REQUEST_URI']
+		);
+	}
 }
